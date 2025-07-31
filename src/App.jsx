1f6170a9d1a43dfcd79b9a8947623d03924cb5b2
2004piyush <153434663+2004piyush@users.noreply.github.com>
2025-08-01 01:17:46 +0530

    Create missing App.jsx component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Job Portal</h1>
+      <p>Welcome to the job portal application!</p>
+    </div>
+  );
+}
+
+export default App;
